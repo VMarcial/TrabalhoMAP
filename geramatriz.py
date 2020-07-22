b9e797f1d6f53fc5ebb4ba9131fb111d15b6e00c
Lucas Giacone <68192656+lucasGiacone@users.noreply.github.com>
2020-07-22 17:25:57 -0300

    Otimizações de tempo e gerador de matrizes simetricas(Cacique-tribo)

--- geramatriz.py
+++ geramatriz.py
@@ -1,34 +1,49 @@
 import random
-def geramatriz(tamanho):
+def geramatriz(tamanho,simetrica):
     matriz = [[0 for i in range(tamanho)] for j in range(tamanho)]
-    for i in range(tamanho):
-        numerodeligacoes = random.randrange(tamanho-1)+1
-        Ligacoes = []
-        contador = numerodeligacoes
-        while contador > 0:
-            numLigação = random.randrange(tamanho)+1
-            comparador = (numLigação not in Ligacoes) and (numLigação != (i+1))
-            Ligacoes.append( (comparador)*numLigação )
-            contador -= comparador
-        while 0 in Ligacoes:
-            Ligacoes.remove(0)
-        for k in Ligacoes:
-            matriz[k-1][i] += 1/numerodeligacoes
-    return matriz
+    if not simetrica:
+        for i in range(tamanho):
+            numerodeligacoes = random.randrange(tamanho-1)+1
+            possiveis = [*range(tamanho)]
+            del(possiveis[i])
+            Ligacoes = random.sample(possiveis,numerodeligacoes)
+            for k in Ligacoes:
+                matriz[k][i] += 1/numerodeligacoes
+        return matriz
+    else:
+        for i in range(tamanho-1):
+            numerodeligacoes = random.randrange(tamanho-1-i)+1
+            possiveis = [*range(i+1,tamanho)]
+            Ligacoes = random.sample(possiveis,numerodeligacoes)
+            for k in Ligacoes:
+                matriz[k][i] += 1
+                matriz[i][k] += 1
+        for i in range(tamanho):
+            divisor = 0
+            for j in range(tamanho):
+                divisor += matriz[j][i]
+            for j in range(tamanho):
+                matriz[j][i] /= divisor
+        return matriz
 
+
+def salva(matriz):
+    arq = open("matriz.txt", "w")
+    arq.write("[")
+    for i in range(tamanho):
+        arq.write("[")
+        for j in range(tamanho):
+            if j != tamanho-1: arq.write(f" {matriz[i][j]:.3f},")
+            else: arq.write(f" {matriz[i][j]:.3f}")
+        if i != tamanho-1: arq.write("],\n ")
+        else: arq.write("]")
+    arq.write("]")
+    arq.close
 
 
 tamanho = int(input("Digite o tamanho da matriz:"))
-matriz = geramatriz(tamanho)
-m = open("matriz.txt", "w")
-m.write("[")
-for i in range(tamanho):
-    m.write("[")
-    for j in range(tamanho):
-        if j != tamanho-1: m.write(f" {matriz[i][j]:.2f},")
-        else: m.write(f" {matriz[i][j]:.2f}")
-    if i != tamanho-1: m.write("],")
-    else: m.write("]")
-m.write("]")
-m.close
+simetrica = int(input("Simetrica(1/0):"))
+matriz = geramatriz(tamanho,simetrica)
 print("gerado")
+salva(matriz)
+print("salvo")
