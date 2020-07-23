820229a5b4a924d74956a5fa8d42c5f6a3429ac5
Lucas Giacone <68192656+lucasGiacone@users.noreply.github.com>
2020-07-23 01:40:58 -0300

    Controle da precisão dos dados

--- geramatriz.py
+++ geramatriz.py
@@ -2,39 +2,40 @@ import random
 def geramatriz(tamanho,simetrica):
     matriz = [[0 for i in range(tamanho)] for j in range(tamanho)]
     if not simetrica:
-        for i in range(tamanho):
-            numerodeligacoes = random.randrange(tamanho-1)+1
+        for coluna in range(tamanho):
+            numeroDeLigacoes = random.randrange(tamanho-1)+1
+            print(numeroDeLigacoes)
             possiveis = [*range(tamanho)]
-            del(possiveis[i])
-            Ligacoes = random.sample(possiveis,numerodeligacoes)
-            for k in Ligacoes:
-                matriz[k][i] += 1/numerodeligacoes
+            del(possiveis[coluna])
+            ligacoes = random.sample(possiveis,numeroDeLigacoes)
+            for linha in ligacoes:
+                matriz[linha][coluna] += 1/numeroDeLigacoes
         return matriz
     else:
-        for i in range(tamanho-1):
-            numerodeligacoes = random.randrange(tamanho-1-i)+1
-            possiveis = [*range(i+1,tamanho)]
-            Ligacoes = random.sample(possiveis,numerodeligacoes)
-            for k in Ligacoes:
-                matriz[k][i] += 1
-                matriz[i][k] += 1
-        for i in range(tamanho):
+        for coluna in range(tamanho-1):
+            numeroDeLigacoes = random.randrange(tamanho-1-coluna)+1
+            possiveis = [*range(coluna+1,tamanho)]
+            ligacoes = random.sample(possiveis,numeroDeLigacoes)
+            for linha in ligacoes:
+                matriz[linha][coluna] += 1
+                matriz[coluna][linha] += 1
+        for coluna in range(tamanho):
             divisor = 0
-            for j in range(tamanho):
-                divisor += matriz[j][i]
-            for j in range(tamanho):
-                matriz[j][i] /= divisor
+            for linha in range(tamanho):
+                divisor += matriz[linha][coluna]
+            for linha in range(tamanho):
+                matriz[linha][coluna] /= divisor
         return matriz
 
 
-def salva(matriz):
+def salva(matriz,tamanho):
     arq = open("matriz.txt", "w")
     arq.write("[")
     for i in range(tamanho):
         arq.write("[")
         for j in range(tamanho):
-            if j != tamanho-1: arq.write(f" {matriz[i][j]:.3f},")
-            else: arq.write(f" {matriz[i][j]:.3f}")
+            if j != tamanho-1: arq.write(f" {matriz[i][j]:.{precisao}f},")
+            else: arq.write(f" {matriz[i][j]:.{precisao}f}")
         if i != tamanho-1: arq.write("],\n ")
         else: arq.write("]")
     arq.write("]")
@@ -43,7 +44,8 @@ def salva(matriz):
 
 tamanho = int(input("Digite o tamanho da matriz:"))
 simetrica = int(input("Simetrica(1/0):"))
+precisao = int(input("Casas depois da virgula:"))
 matriz = geramatriz(tamanho,simetrica)
 print("gerado")
-salva(matriz)
+salva(matriz,tamanho)
 print("salvo")
